8918bfdfeff7ea396ab7cbd05b55d340d1c7178a
Frederico Malaquias <91347722+Fredaumskrt@users.noreply.github.com>
2023-01-09 19:15:14 -0300

    Add files via upload

---
+++ EP04/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    clock_t inicio, fim, T;
+    float Tempo, media=0;
+    register int c;
+    //altera aqui quando for int, float ou char
+    float k, i = 0, j = 3;
+//    float *i, *j,x=3,y=1;
+//    i=&x;
+//    j=&y;
+    // int k, num1=1, num2=3;
+    T=CLOCKS_PER_SEC;
+for (k=1;k<=10;k=k+1)
+{   inicio=clock();
+// constante = i = i + 3
+
+// variavel = i = i + j
+
+// tempo base deixa em i = j
+    for (c=1;c<=10000000;c=c+1) i = i || 3 ;
+    fim = clock();
+    Tempo =( (fim - inicio)*1000/CLOCKS_PER_SEC);
+    printf("\nTempo : %g ms.", Tempo);
+    media=media+Tempo;
+}
+    printf("\nTempo gasto media: %g ms.", media/10);
+
+}
